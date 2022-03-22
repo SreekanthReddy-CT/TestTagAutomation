a23d0d88278e052f14f8e8cf4da899ffd5aa0fbc
Sreekanth Reddy <sreekanth.reddy@ymedialabs.com>
2022-03-22 10:29:54 +0530

    First commit

---
+++ TestTagAutomation/TestTagAutomation.h
@@ -0,0 +1,18 @@
+//
+//  TestTagAutomation.h
+//  TestTagAutomation
+//
+//  Created by YML on 22/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestTagAutomation.
+FOUNDATION_EXPORT double TestTagAutomationVersionNumber;
+
+//! Project version string for TestTagAutomation.
+FOUNDATION_EXPORT const unsigned char TestTagAutomationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestTagAutomation/PublicHeader.h>
+
+
